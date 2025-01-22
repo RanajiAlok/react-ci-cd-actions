6b959526f0fe64ef2ba9f7e79a67b504965e6081
Alok Kumar Singh <86158900+RanajiAlok@users.noreply.github.com>
2025-01-22 11:40:45 +0530

    added new branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <h1>Github Actions</h1>
       <h2>Complete course with 2 projects</h2>
+      <h3>With Sonar Cloud </h3>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
